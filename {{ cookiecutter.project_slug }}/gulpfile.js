716d42abf584d72991456f30d360195c4c75b281
Anand Magaji <anhedonix@gmail.com>
2019-11-01 16:24:17 +0530

    PurgeCss implemented

--- {{ cookiecutter.project_slug }}/gulpfile.js
+++ {{ cookiecutter.project_slug }}/gulpfile.js
@@ -3,26 +3,27 @@
 ////////////////////////////////
 
 // Gulp and package
-const {src, dest, parallel, series, watch} = require('gulp');
-const pjson = require('./package.json');
+const {src, dest, parallel, series, watch} = require('gulp')
+const pjson = require('./package.json')
 
 // Plugins
-const autoprefixer = require('autoprefixer');
-const browserSync = require('browser-sync').create();
+const autoprefixer = require('autoprefixer')
+const browserSync = require('browser-sync').create()
 
-const cssnano = require('cssnano');
-const imagemin = require('gulp-imagemin');
-const pixrem = require('pixrem');
-const plumber = require('gulp-plumber');
-const postcss = require('gulp-postcss');
-const reload = browserSync.reload;
-const rename = require('gulp-rename');
-const sass = require('gulp-sass');
-const spawn = require('child_process').spawn;
-const uglify = require('gulp-uglify-es').default;
-const request = require('request');
-const waitOn = require('wait-on');
-const concat = require('gulp-concat');
+const cssnano = require('cssnano')
+const imagemin = require('gulp-imagemin')
+const pixrem = require('pixrem')
+const plumber = require('gulp-plumber')
+const postcss = require('gulp-postcss')
+const reload = browserSync.reload
+const rename = require('gulp-rename')
+const sass = require('gulp-sass')
+const spawn = require('child_process').spawn
+const uglify = require('gulp-uglify-es').default
+const request = require('request')
+const waitOn = require('wait-on')
+const concat = require('gulp-concat')
+const purgecss = require('gulp-purgecss')
 
 // Relative paths function
 function pathsConfig(appName) {
@@ -69,6 +70,11 @@ function styles() {
         }).on('error', sass.logError))
         .pipe(plumber()) // Checks for errors
         .pipe(postcss(processCss))
+        .pipe(
+          purgecss({
+            content: [`${paths.templates}/**/*.html`]
+          })
+        )
         .pipe(dest(paths.css))
         .pipe(rename({suffix: '.min'}))
         .pipe(postcss(minifyCss)) // Minifies the result
